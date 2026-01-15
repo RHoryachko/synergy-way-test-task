3363332ed0d96f23cd3f7b11416a2366864e03cd
RHoryachko <rhoryachko@gmail.com>
2026-01-15 15:35:00 +0200

    Restore requirements.txt with all dependencies

--- app/config.py
+++ app/config.py
@@ -1,8 +1,15 @@
-from pydantic_settings import BaseSettings
+import os
 
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql://synergy:synergy123@localhost:5432/synergy_db"
+    database_url: str = Field(default=f"postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/${POSTGRES_DB}")
     redis_url: str = "redis://localhost:6379/0"
     jsonplaceholder_url: str = "https://jsonplaceholder.typicode.com"
     dummyjson_url: str = "https://dummyjson.com"
